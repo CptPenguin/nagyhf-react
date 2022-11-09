19f60bda80e4e1121615ac368f6bbd06f11194e5
CptPenguin <32851658+CptPenguin@users.noreply.github.com>
2022-11-09 11:25:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nagyhf-react
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-muz2hf)
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.css';
+
+//1. feladat
+
+export default function App() {
+  return <div></div>;
+}
