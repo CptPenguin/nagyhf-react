d05845b06fdc47bb4382cc47164e273e1b0472bd
CptPenguin <32851658+CptPenguin@users.noreply.github.com>
2022-11-24 12:41:36 +0100

    2 files modified

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,45 @@
 import React, { useState } from 'react';
 import { LoginFail } from './LoginFail.js';
+import styled from '@emotion/styled';
+
+const Loginbase = styled.div`
+position: fixed;
+  inset: 0;
+  background-color: rgba(100, 100, 100, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const loginWindow = styled.div`
+padding: 50px;
+display: flex;
+flex-direction: column;
+background-color: white;
+border: 3px solid black;
+border-radius: 20px;
+`;
+
+const inputDiv = styled.div`
+  border: 5px solid black;
+`;
+const inputField = styled.input`
+border: 0px;
+`;
 
 export function Login({ setLoginScreen }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   //const [onButtonPressLoginFail, setOnButtonPressLoginFail] = useState(false);
   return (
-    <div className="Loginbase">
-      <div className="Loginwindow">
-        <div>
+    <Loginbase>
+      <loginWindow>
+        <inputDiv>
           Email:
-          <input
-            onChange={(event) => 
-              setEmail(event.target.value)
-            }
-          ></input>
-        </div>
+          <inputField
+            onChange={(event) => setEmail(event.target.value)}
+          ></inputField>
+        </inputDiv>
         <div>
           Password:
           <input
@@ -26,15 +50,14 @@ export function Login({ setLoginScreen }) {
         </div>
         <button
           onClick={() => {
-            email!=="" && password!==""
+            email !== '' && password !== ''
               ? setLoginScreen(false)
-              : console.log("Email or password missing!")//setOnButtonPressLoginFail(false);
+              : console.log('Email or password missing!'); //setOnButtonPressLoginFail(false);
           }}
         >
           Log in
         </button>
-        
-      </div>
-    </div>
+      </loginWindow>
+    </Loginbase>
   );
 }
