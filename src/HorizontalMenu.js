2b705db8879d120d2fa46c7a06f27f52a319d029
CptPenguin <32851658+CptPenguin@users.noreply.github.com>
2022-11-24 12:35:57 +0100

    1 file modified

--- src/HorizontalMenu.js
+++ src/HorizontalMenu.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { css } from '@emotion/react';
-import styled from '@emotion.styled';
+import styled from '@emotion/styled';
 
 export function Horizontal({
   setLoginScreen,
